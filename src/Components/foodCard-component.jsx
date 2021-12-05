678f3bb60dc60ca62f80f8518e6e2ca7be8d8cbf
stavramovic <stefan.avramovic096@gmail.com>
2021-12-05 20:41:49 +0100

    small improvements

--- src/Components/foodCard-component.jsx
+++ src/Components/foodCard-component.jsx
@@ -15,114 +15,116 @@ const FoodCardComponent = ({title, content}) => {
         <>
             <div className='title' id={title.replace(' ','-')}>{title}</div>
             <div className='card'>
-                <div>{content.map(info => {
-                    return (
-                        <Card 
-                            sx={{
-                                width: 700,
-                                height: 260, 
-                                margin: 8,
-                                display: 'flex'
-                            }}
-                            key={info.id}
-                        >
-                            <CardMedia
+                <div>
+                    {content.map(info => {
+                        return (
+                            <Card 
                                 sx={{
-                                    width: 280,
-                                    height: 260
+                                    width: 650,
+                                    height: 250, 
+                                    margin: 8,
+                                    display: 'flex'
                                 }}
-                                component='img'
-                                image={info.img}
-                                alt=''
-                            />
-                            <CardContent 
-                                sx={{
-                                    position: 'relative',
-                                    width: '100%',
-                                    display: 'flex',
-                                    flexDirection: 'column',
-                                    alignItems: 'center',
-                                    paddingTop: '10px',
-                                    textAlign: 'center'
-                                }}
+                                key={info.id}
                             >
-                                <Typography 
-                                    gutterBottom variant='h5'
-                                    component='div'
-                                >
-                                    {info.name}
-                                </Typography>
-                                        
-                                <Typography 
-                                    variant='body2'
-                                    color='text.secondary'
+                                <CardMedia
                                     sx={{
-                                        width: '80%', 
-                                        textAlign: 'center',
-                                        padding: '15px'
+                                        width: 280,
+                                        height: 250,
+                                        objectFit: 'fill'
+                                    }}
+                                    component='img'
+                                    image={info.img}
+                                    alt=''
+                                />
+                                <CardContent 
+                                    sx={{
+                                        position: 'relative',
+                                        width: '100%',
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        alignItems: 'center',
+                                        paddingTop: '10px',
+                                        textAlign: 'center'
                                     }}
                                 >
-                                </Typography>
-                                <List 
-                                    sx={{
-                                        width: '90%',
-                                        bgcolor: 'background.paper', 
-                                        padding: '0', 
-                                        display: 'flex', 
-                                        flexDirection: 'column', 
-                                        alignItems: 'flex-end'
-                                    }}
-                                >
-                                    {info.size?.map((item, index) => (
-                                        <ListItem
-                                            sx={{padding: '4px'}}
-                                            key={index}
-                                            disableGutters
-                                            secondaryAction={
-                                                <Button 
-                                                    variant='outlined'
-                                                    sx={{
-                                                        color: 'rgb(249, 105, 14)',
-                                                        '&:hover': {backgroundColor: 'rgba(249, 105, 14, 0.1)', border: '1px solid rgb(249, 105, 14)'},
-                                                        border: '1px solid rgb(249, 105, 14)',
-                                                        width: '80px',
-                                                        fontSize: '14px',
-                                                        padding: '0',
-                                                    }}
-                                                >
-                                                    {info.price[index]} 
-                                                </Button>
-                                            }
-                                        >
-                                            <ListItemText 
-                                                primaryTypographyProps={{
-                                                    fontSize: '14px'
-                                                }}  
-                                                primary={item} 
-                                            />
-                                        </ListItem>
-                                    ))}
-                                    {info.size === undefined && 
-                                        <Button
-                                            variant='outlined'
-                                            sx={{
-                                                color: 'rgb(249, 105, 14)',
-                                                '&:hover': {backgroundColor: 'rgba(249, 105, 14, 0.1)', border: '1px solid rgb(249, 105, 14)'},
-                                                border: '1px solid rgb(249, 105, 14)',
-                                                width: '80px',
-                                                fontSize: '14px',
-                                                padding: '0',
-                                                marginTop: '30px'
-                                            }}
-                                        >
-                                            {info.price}
-                                        </Button>
-                                    }
-                                </List>
-                            </CardContent> 
-                        </Card>
-                    );
-                })}
+                                    <Typography 
+                                        gutterBottom variant='h5'
+                                        component='div'
+                                    >
+                                        {info.name}
+                                    </Typography>
+                                            
+                                    <Typography 
+                                        variant='body2'
+                                        color='text.secondary'
+                                        sx={{
+                                            width: '80%', 
+                                            textAlign: 'center',
+                                            padding: '15px'
+                                        }}
+                                    >
+                                    </Typography>
+                                    <List 
+                                        sx={{
+                                            width: '100%',
+                                            bgcolor: 'background.paper', 
+                                            padding: '0', 
+                                            display: 'flex', 
+                                            flexDirection: 'column', 
+                                            alignItems: 'flex-end'
+                                        }}
+                                    >
+                                        {info.size?.map((item, index) => (
+                                            <ListItem
+                                                sx={{padding: '5px'}}
+                                                key={index}
+                                                disableGutters
+                                                secondaryAction={
+                                                    <Button 
+                                                        variant='outlined'
+                                                        sx={{
+                                                            color: 'rgb(249, 105, 14)',
+                                                            '&:hover': {backgroundColor: 'rgba(249, 105, 14, 0.1)', border: '1px solid rgb(249, 105, 14)'},
+                                                            border: '1px solid rgb(249, 105, 14)',
+                                                            width: '80px',
+                                                            fontSize: '14px',
+                                                            padding: '0',
+                                                        }}
+                                                    >
+                                                        {info.price[index]} 
+                                                    </Button>
+                                                }
+                                            >
+                                                <ListItemText 
+                                                    primaryTypographyProps={{
+                                                        fontSize: '14px'
+                                                    }}  
+                                                    primary={item} 
+                                                />
+                                            </ListItem>
+                                        ))}
+                                        {info.size === undefined && 
+                                            <Button
+                                                variant='outlined'
+                                                sx={{
+                                                    color: 'rgb(249, 105, 14)',
+                                                    '&:hover': {backgroundColor: 'rgba(249, 105, 14, 0.1)', border: '1px solid rgb(249, 105, 14)'},
+                                                    border: '1px solid rgb(249, 105, 14)',
+                                                    width: '80px',
+                                                    fontSize: '14px',
+                                                    padding: '0',
+                                                    marginTop: '30px'
+                                                }}
+                                            >
+                                                {info.price}
+                                            </Button>
+                                        }
+                                    </List>
+                                </CardContent> 
+                            </Card>
+                        );
+                    })}
                 </div>
             </div>
         </>
