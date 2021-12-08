069ee949a8324c153dd560140be29a190f25127d
stavramovic <stefan.avramovic096@gmail.com>
2021-12-08 01:03:27 +0100

    passing info up the chain

--- src/Components/foodSection.jsx
+++ src/Components/foodSection.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import FoodCard from './foodCard';
+import FoodCard from './FoodCard/foodCard';
 
 
-export default function FoodSection() {
+export default function FoodSection(props) {
+
+    const foodCardDataHandler = foodCard => {
+        props.foodSectionData(foodCard)
+    }
+
     return (
         <div className='foodSection'>
-            <FoodCard />
+            <FoodCard foodCardData={foodCardDataHandler} />
         </div>
     );
 }
