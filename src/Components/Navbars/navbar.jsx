a6d68665aa36a46923430db3ac02c29896ac2d9e
stavramovic <stefan.avramovic096@gmail.com>
2021-12-16 03:19:03 +0100

    navbar update

--- src/Components/Navbars/navbar.jsx
+++ src/Components/Navbars/navbar.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
-import SearchBar from 'material-ui-search-bar';
 import BlackButton from '../Button/blackButton.jsx';
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
 
 
 export default function Navbar() {
     return (
         <nav className='navbar'>
             <a className='logo' href='/'>Restaurant</a>
-            <SearchBar 
-                style={{
-                    height: 40, 
-                    width: 400, 
-                    backgroundColor: '#f7f7f7'
-                }} 
-            />
+            <Paper
+                component="form"
+                sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+            >
+                <InputBase
+                    sx={{ ml: 1, flex: 1 }}
+                    placeholder="Search"
+                />
+                <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
+                    <SearchIcon />
+                </IconButton>
+            </Paper>
             <div className='loginButton'>
                 <BlackButton  param='Prijava'/>
                 <BlackButton  param='Registracija'/>
